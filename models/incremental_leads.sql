4061af3a98457b9577de4ce4cd78779d53764b0d
Nikhil Tungaturthy <nikhil.tungaturthy@dbtlabs.com>
2026-08-11 02:01:43 +0000

    Salesforce

--- models/incremental_leads.sql
+++ models/incremental_leads.sql
@@ -1,54 +1,43 @@
 {{
     config(
         materialized='incremental',
-        unique_key='lead_id'
+        unique_key='lead_id',
+        incremental_strategy='merge'
     )
 }}
 
-with new_leads as (
+with source_leads as (
 
     select
-    *
-    from
-    {{ source('salesforce','lead') }}
+        id as lead_id,
+        first_name,
+        last_name,
+        title,
+        company,
+        email,
+        phone,
+        street,
+        city,
+        state,
+        postal_code,
+        country,
+        created_date,
+        last_modified_date,
+        lead_source,
+        status,
+        industry
+    from {{ source('salesforce', 'lead') }}
+    where not is_deleted
+      and not is_converted
 
     {% if is_incremental() %}
-
-    where created_date >= ( select max(created_date) from {{ this }} )
-
+      and last_modified_date >= (
+          select coalesce(max(last_modified_date), '1900-01-01'::timestamp_ntz)
+          from {{ this }}
+      )
     {% endif %}
 
 )
 
-select
-id as lead_id
-, first_name
-, last_name
-, title
-, company
-, '2945 Parfet Drive, Lakewood, Colorado 80124' as address
-, created_date
-, lead_source
-, status
-, industry
--- , rating
-, DATEADD(day, -(UNIFORM(1, 365, RANDOM())), current_date()) AS most_recent_website_visit
-, DATEADD(day, -(UNIFORM(1, 365, RANDOM())), most_recent_website_visit) AS first_website_visit_date
-, (select 
-    substr(email, 1, position('.com' in email) +3 ) 
-    from census.salesforce_sandbox_ts_census.user
-    order by random()
-    limit 1) as assigned_rep
-, case
-    when day(first_website_visit_date) > 15 then true
-    else false
-    end as has_purchase_history
-, case
-    when has_purchase_history = true then uniform(0,50000, random())
-    else 0
-    end as lifetime_value
-from new_leads
-where
-not is_converted
-and
-not is_deleted
+select *
+from source_leads
